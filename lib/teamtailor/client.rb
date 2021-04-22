1dbbd136d1351060ba9767385bba4deb9934c8dc
Daniel Kittilsen Henriksen <danielkhenriksen@gmail.com>
2021-04-23 00:51:40 +0200

    Rubocop

--- lib/teamtailor/client.rb
+++ lib/teamtailor/client.rb
@@ -93,7 +93,7 @@ module Teamtailor
           body: {
               data: {
                   type: "uploads",
-                  attributes: attributes.transform_keys{ |k| k.to_s.gsub("_", "-") },
+                  attributes: attributes.transform_keys { |k| k.to_s.gsub("_", "-") },
                   relationships: relationships,
               },
           }
