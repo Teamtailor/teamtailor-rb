2fec066e700ca1010063887f8151c351f00b1f65
vbiletskii <vitalii.biletskiy@accbox.info>
2025-04-08 13:16:14 +0300

    Added better error handling

--- lib/teamtailor/error.rb
+++ lib/teamtailor/error.rb
@@ -3,24 +3,38 @@
 module Teamtailor
   class Error < StandardError
     def self.from_response(body:, status:)
+      json_response = parse_body(body)
+
+      error_message = json_response.dig("errors", 0, "detail") || json_response.dig("errors", "detail") || "Unknown error"
+
       case status
       when 401
         Teamtailor::UnauthorizedRequestError.new
       when 406
-        json_response = JSON.parse(body)
-        Teamtailor::InvalidApiVersionError.new(
-          json_response.dig("errors", "detail")
-        )
+        Teamtailor::InvalidApiVersionError.new(error_message)
       when 422
-        json_response = JSON.parse(body)
-        Teamtailor::UnprocessableEntityError.new(
-            json_response.dig("errors", 0, "detail")
-        )
+        Teamtailor::UnprocessableEntityError.new(error_message)
+      when 400..499
+        ClientError.new(error_message)
+      when 500..599
+        ServerError.new(error_message)
+      else
+        UnknownResponseError.new("Unexpected error (status: #{status})")
+      end
+    end
+
+    def self.parse_body(body)
+      begin
+        JSON.parse(body)
+      rescue JSON::ParserError => e
+        {}
       end
     end
   end
 
   class ClientError < Error; end
+
+  class ServerError < Error; end
 
   class UnauthorizedRequestError < ClientError; end
 
@@ -33,4 +47,6 @@ module Teamtailor
   class UnloadedRelationError < ClientError; end
 
   class UnprocessableEntityError < ClientError; end
+
+  class UnknownResponseError < Error; end
 end
